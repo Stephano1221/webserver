06dc244c9693e3ac9c1fddd444e37e6e56e121ec
Nathan Cole <46795318+Stephano1221@users.noreply.github.com>
2024-11-01 23:23:04 +0000

    #10 Add support for `Content-Type` header

--- src/http_parser/http_fieldname.rs
+++ src/http_parser/http_fieldname.rs
@@ -3,15 +3,17 @@ use core::fmt;
 #[derive(Clone, PartialEq, Eq, Hash)]
 pub enum HttpFieldName {
     ContentLength,
-    Host
+    ContentType,
+    Host,
 }
 
 impl HttpFieldName {
     pub fn from_str(field_name: &str) -> Option<Self> {
         // As field names are case-insensitive, `field_name` and the cases below must be the same case
         match field_name.trim().to_ascii_lowercase().as_str() {
-            "host" => Some(Self::Host),
             "content-length" => Some(Self::ContentLength),
+            "content-type" => Some(Self::ContentType),
+            "host" => Some(Self::Host),
             _ => None
         }
     }
@@ -20,8 +22,9 @@ impl HttpFieldName {
 impl fmt::Display for HttpFieldName {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let field_name = match self {
+            Self::ContentLength => "Content-Length",
+            Self::ContentType => "Content-Type",
             Self::Host => "Host",
-            Self::ContentLength => "Content-Length"
         };
         write!(f, "{field_name}")
     }
